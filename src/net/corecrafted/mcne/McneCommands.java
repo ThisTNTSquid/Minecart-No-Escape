66fbaa9dda9efaa5f93293db4cde5213a08973bc
ThisTNTSquid <tntsquid@corecrafted.net>
2017-03-25 17:26:52 +0800

    Everything's fine now
    well in fact, it do

--- src/net/corecrafted/mcne/McneCommands.java
+++ src/net/corecrafted/mcne/McneCommands.java
@@ -24,8 +24,6 @@ public class McneCommands implements CommandExecutor {
 
 	public static ArrayList<Player> lockedPlayer = new ArrayList<Player>();
 
-	
-
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		double range = plugin.getConfig().getDouble("near-area");
@@ -35,40 +33,58 @@ public class McneCommands implements CommandExecutor {
 			return true;
 		} else if (args.length == 1) {
 
+			if (args[0].equals("list")) {
+				sender.sendMessage(ChatColor.YELLOW + "Current Minecart Locked Player: \n");
+				for (Player p : lockedPlayer) {
+					sender.sendMessage(ChatColor.GRAY + "- " + ChatColor.GOLD + p.getName() + "\n");
+				}
+			}
+
 			if (args[0].equals("lock") || args[0].equals("unlock") || args[0].equals("toggle")) {
 				sender.sendMessage(
 						McneMain.prefix + ChatColor.RED + " Please select a target player [playername/-near]");
 				return true;
+			} else {
+				if (!(args[0].equals("list"))) {
+					sender.sendMessage(
+							McneMain.prefix + ChatColor.RED + " Please specify a proper action [lock/unlock/toggle]");
+					return true;
+				}
 			}
-		} else {
-			sender.sendMessage(
-					McneMain.prefix + ChatColor.RED + " Please specify a proper action [lock/unlock/toggle]");
-			return true;
+
 		}
 		if (args.length == 2) {
 			if (args[0].equals("lock")) {
 				if (args[1].equals("-near")) {
-					addnearToLock(sender,range);
+					addnearToLock(sender, range);
+					sender.sendMessage(McneMain.prefix + ChatColor.GREEN + " Add - OK");
 				} else {
 					Player target = Bukkit.getPlayer(args[1]);
-					lockedPlayer.add(target);
+					if (target == null) {
+						sender.sendMessage(McneMain.prefix + ChatColor.RED + " Add - The player is not online");
+					} else {
+						lockedPlayer.add(target.getPlayer());
+						sender.sendMessage(McneMain.prefix + ChatColor.GREEN + " Add - OK");
+					}
 				}
 				return true;
 			}
-			if (args[0].equals("unlock")){
-				if (args[1].equals("-near")){
-					removeFromNear(sender,range);
+			if (args[0].equals("unlock")) {
+				if (args[1].equals("-near")) {
+					removeFromNear(sender, range);
+					sender.sendMessage(McneMain.prefix + ChatColor.GREEN + " Remove - OK");
 				} else {
 					Player target = Bukkit.getPlayer(args[1]);
-					lockedPlayer.remove(target);
+					if (target == null) {
+						sender.sendMessage(McneMain.prefix + ChatColor.RED + " Remove - The player is not online");
+					} else {
+						lockedPlayer.remove(target.getPlayer());
+						sender.sendMessage(McneMain.prefix + ChatColor.GREEN + " Remove - OK");
+					}
 				}
 				return true;
 			}
-			if (args[0].equals("debug")){
-				for (Player p: lockedPlayer){
-					sender.sendMessage(p.getName()+"\n");
-				}
-			}
+
 		}
 
 		return true;
@@ -101,8 +117,8 @@ public class McneCommands implements CommandExecutor {
 			sender.sendMessage(McneMain.prefix + ChatColor.RED + " You must be in the game to execute this");
 		}
 	}
-	
-	private void removeFromNear(CommandSender sender ,double nearRange){
+
+	private void removeFromNear(CommandSender sender, double nearRange) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			Location loc = player.getLocation();
