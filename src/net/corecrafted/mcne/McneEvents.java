3ca9befb86f4e8bacee5943f3905e554c5a1b7dc
ThisTNTSquid <tntsquid@corecrafted.net>
2017-03-25 17:41:11 +0800

    Last: Should be ok
    this build should work

--- src/net/corecrafted/mcne/McneEvents.java
+++ src/net/corecrafted/mcne/McneEvents.java
@@ -1,9 +1,23 @@
 package net.corecrafted.mcne;
 
+import org.bukkit.entity.Player;
+import org.bukkit.entity.minecart.RideableMinecart;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleExitEvent;
 
 public class McneEvents implements Listener {
 
-	public void onDismount() {
+	@EventHandler
+	public void onDismount(VehicleExitEvent e) {
+		if (e.getVehicle() instanceof RideableMinecart){
+			if (e.getExited() instanceof Player){
+				Player player = (Player) e.getExited();
+				if (McneCommands.lockedPlayer.contains(player)){
+					e.setCancelled(true);
+				}
+					
+			}
+		}
 	}
 }
